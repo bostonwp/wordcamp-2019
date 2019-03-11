e931153b592a249aec6e39db6fd6ac546cd7e194
Kelly Dwan <ryelle@users.noreply.github.com>
2019-03-11 17:30:35 -0400

    Update variables, remove colors from base sass

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ require_once __DIR__ . '/mock-widget-subscriptions.php';
 
 /**
  * Force-set some widgets by visiting
- *	[URL]/wp-admin/admin-ajax.php?action=b18-force-widgets
+ *	[URL]/wp-admin/admin-ajax.php?action=b19-force-widgets
  */
 function boston_2019_force_widgets() {
 	$sidebars = []; //get_option( 'sidebars_widgets' );
@@ -39,44 +39,59 @@ function boston_2019_force_widgets() {
 	$banner = array(
 		'title' => '',
 		'content' =>
-			'<div class="level-1">Two days of WordPress design, development, marketing, content, and business.</div>' .
-			'<div class="level-2"><strong>July 20&ndash;21<sup>nd</sup> 2019</strong><br />Boston University</div>',
-		'filter' => false,
-	);
-	$actions = array(
-		'title' => '',
-		'content' =>
-			'<a href="#" class="button">Register</a>' .
-			'<a href="#" class="button">Volunteer</a>' .
-			'<a href="#" class="button">Speak</a>',
+			'<div class="level-1">Celebrating 10 years</div>' .
+			'<div class="level-2">2010-2019</div>' .
+			'<div class="level-4">Dates</div>' .
+			'<div class="level-3">July 21-22nd 2019</div>' .
+			'<div class="level-4">Location</div>' .
+			'<div class="level-3">Boston University</div>' .
+			'<div class="level-4">Price</div>' .
+			'<div class="level-3">$50</div>' .
+			'<div class="button-row">' .
+			'	<a href="#" class="button">Register</a>' .
+			'	<a href="#" class="button">Volunteer</a>' .
+			'</div>',
 		'filter' => false,
 	);
 	$sponsors = array(
-		'title' => 'Platinum Sponsors',
+		'title' => 'Thank you to our Platinum Sponsors',
 	);
-	$inner_actions = array(
+	$subscribe = array();
+	$nav_menu_1 = array( 'nav_menu' => 20 );
+	$nav_menu_2 = array( 'nav_menu' => 21 );
+	$social_links = array(
 		'title' => '',
 		'content' =>
-			'<div class="label">Looking for speakers!</div>' .
-			'<div class="action"><a href="#" class="button">Apply to speak</a></div>',
+			'<div class="social-links-row">' .
+			'	<ul>' .
+			'		<li><a href="https://twitter.com/wordcampboston">twitter: @wordcampboston</a></li>' .
+			'		<li><a href="https://facebook.com">WordCamp Boston facebook page</a></li>' .
+			'		<li><a href="https://youtube.com">WordCamp Boston youtube channel</a></li>' .
+			'		<li><a href="https://meetup.com">Boston WordPress meetup</a></li>' .
+			'		<li><a href="https://bostonwp.org">BostonWP.org</a></li>' .
+			'	</ul>' .
+			'</div>' .
+			'<p>Proudly Powered by WordPress</p>' .
+			'<p>Go to WordCamp Central</p>',
 		'filter' => false,
 	);
-	$subscribe = [];
 
-	$widget_text = array( 1 => $banner, 2 => $actions, 3 => $inner_actions, '_multiwidget' => 1 );
+	$widget_text = array( 1 => $banner, 2 => $social_links, '_multiwidget' => 1 );
 	$widget_sponsors = array( 1 => $sponsors, '_multiwidget' => 1 );
+	$widget_menus = array( 1 => $nav_menu_1, 2 => $nav_menu_2, '_multiwidget' => 1 );
 	$widget_subscription = array( 1 => $subscribe, '_multiwidget' => 1 );
 
 	update_option( 'widget_custom_html', $widget_text );
 	update_option( 'widget_wcb_sponsors', $widget_sponsors );
+	update_option( 'widget_nav_menu', $widget_menus );
 	update_option( 'widget_mock_subscription_widget', $widget_subscription );
 
-	$sidebars['before-content-homepage-1'] = [ 'custom_html-1', 'custom_html-2' ];
-	$sidebars['footer-1'] = [ 'mock_subscription_widget-1' ];
-	$sidebars['sidebar-1'] = [ 'custom_html-3', 'wcb_sponsors-1' ];
+	$sidebars['before-content-homepage-1'] = [ 'custom_html-1' ];
+	$sidebars['sidebar-1'] = [ 'mock_subscription_widget-1', 'wcb_sponsors-1' ];
+	$sidebars['footer-1'] = [ 'nav_menu-1', 'nav_menu-2', 'custom_html-2' ];
 
 	update_option( 'sidebars_widgets', $sidebars );
 
 	wp_die();
 }
-add_action( 'wp_ajax_b18-force-widgets', 'boston_2019_force_widgets' );
+add_action( 'wp_ajax_b19-force-widgets', 'boston_2019_force_widgets' );
