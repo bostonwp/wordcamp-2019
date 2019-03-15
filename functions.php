b18d183b9f5daa582ee90c47148b721af8f95997
Kelly Dwan <ryelle@users.noreply.github.com>
2019-03-14 23:52:38 -0400

    Add extra widgets to the population script

--- functions.php
+++ functions.php
@@ -59,36 +59,86 @@ function boston_2019_force_widgets() {
 	$subscribe = array();
 	$nav_menu_1 = array( 'nav_menu' => 20 );
 	$nav_menu_2 = array( 'nav_menu' => 21 );
+	$news_header = array(
+		'title' => '',
+		'content' => '<h1 class="page-title">News</h1>',
+		'filter' => false,
+		'conditions' => array (
+			'action' => 'show',
+			'match_all' => '0',
+			'rules' => array (
+				array (
+					'major' => 'page',
+					'minor' => 'post_type-post',
+					'has_children' => false,
+				),
+			),
+		),
+	);
+
+	$social_links_content = <<<EOF
+<div class="social-links-row">
+<ul>
+	<li><a class="wc-icon-twitter" href="https://twitter.com/wordcampboston">
+		<span class="screen-reader-text">twitter: @wordcampboston</span>
+	</a></li>
+	<li><a class="wc-icon-facebook" href="https://facebook.com">
+		<span class="screen-reader-text">WordCamp Boston facebook page</span>
+	</a></li>
+	<li><a class="wc-icon-youtube" href="https://youtube.com">
+		<span class="screen-reader-text">WordCamp Boston youtube channel</span>
+	</a></li>
+	<li><a class="wc-icon-meetup" href="https://meetup.com">
+		<span class="screen-reader-text">Boston WordPress meetup</span>
+	</a></li>
+	<li><a class="wc-icon-wp" href="https://bostonwp.org">
+		<span class="screen-reader-text">BostonWP.org</span>
+	</a></li>
+</ul>
+</div>
+<p><a class="site-info-generator" href="https://wordpress.org/" rel="generator">
+Proudly powered by WordPress</a><br /><a class="site-info-network" href="https://central.wordcamp.org/">Go to WordCamp Central</a></p>
+EOF;
 	$social_links = array(
 		'title' => '',
-		'content' =>
-			'<div class="social-links-row">' .
-			'	<ul>' .
-			'		<li><a class="icon-twitter" href="https://twitter.com/wordcampboston">twitter: @wordcampboston</a></li>' .
-			'		<li><a class="icon-facebook" href="https://facebook.com">WordCamp Boston facebook page</a></li>' .
-			'		<li><a class="icon-youtube" href="https://youtube.com">WordCamp Boston youtube channel</a></li>' .
-			'		<li><a class="icon-meetup" href="https://meetup.com">Boston WordPress meetup</a></li>' .
-			'		<li><a class="icon-wp" href="https://bostonwp.org">BostonWP.org</a></li>' .
-			'	</ul>' .
-			'</div>' .
-			'<p>Proudly Powered by WordPress</p>' .
-			'<p>Go to WordCamp Central</p>',
+		'content' => $social_links_content,
 		'filter' => false,
 	);
 
-	$widget_text = array( 1 => $banner, 2 => $social_links, '_multiwidget' => 1 );
+	$image = array (
+		'attachment_id' => 332,
+		'url' => 'http://boston.wordcamp.test/wp-content/uploads/sites/48/2019/03/cake.png',
+		'title' => '',
+		'size' => 'full',
+		'width' => 260,
+		'height' => 346,
+		'caption' => '',
+		'alt' => 'WordCamp Boston',
+		'link_type' => 'custom',
+		'link_url' => '',
+		'image_classes' => '',
+		'link_classes' => '',
+		'link_rel' => '',
+		'link_target_blank' => false,
+		'image_title' => '',
+	);
+
+	$widget_text = array( 1 => $banner, 2 => $social_links,  3 => $news_header, '_multiwidget' => 1 );
 	$widget_sponsors = array( 1 => $sponsors, '_multiwidget' => 1 );
 	$widget_menus = array( 1 => $nav_menu_1, 2 => $nav_menu_2, '_multiwidget' => 1 );
 	$widget_subscription = array( 1 => $subscribe, '_multiwidget' => 1 );
+	$widget_media = array( 1 => $image, '_multiwidget' => 1 );
 
 	update_option( 'widget_custom_html', $widget_text );
 	update_option( 'widget_wcb_sponsors', $widget_sponsors );
 	update_option( 'widget_nav_menu', $widget_menus );
 	update_option( 'widget_mock_subscription_widget', $widget_subscription );
+	update_option( 'widget_media_image', $widget_media );
 
 	$sidebars['before-content-homepage-1'] = [ 'custom_html-1' ];
+	$sidebars['before-content-1'] = [ 'custom_html-3' ];
 	$sidebars['sidebar-1'] = [ 'mock_subscription_widget-1', 'wcb_sponsors-1' ];
-	$sidebars['footer-1'] = [ 'nav_menu-1', 'nav_menu-2', 'custom_html-2' ];
+	$sidebars['footer-1'] = [ 'media_image-1', 'nav_menu-1', 'nav_menu-2', 'custom_html-2' ];
 
 	update_option( 'sidebars_widgets', $sidebars );
 
